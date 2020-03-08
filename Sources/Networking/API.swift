a95071430ddac0d4cd6bfe9d231fe2e7e1b9960e
Eugène Peschard <eugene.peschard@dowjones.com>
2020-03-08 22:58:29 +0100

    add errorDescription to APIError

--- Sources/Networking/API.swift
+++ Sources/Networking/API.swift
@@ -21,6 +21,16 @@ public enum APIError: Swift.Error {
     case unexpectedResponse
 }
 
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL: return "Invalid URL"
+        case let .httpCode(code): return "Unexpected HTTP code: \(code)"
+        case .unexpectedResponse: return "Unexpected response from the server"
+        }
+    }
+}
+
 public extension API {
     func urlRequest(baseURL: String,
                     with headers: [String: String]? = nil)
